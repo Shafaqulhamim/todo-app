c9e8758293b2a940a985edd638b62b272012d578
Shafaqulhamim <shafaquli@gmail.com>
2025-01-08 14:32:03 -0600

    Updates

---
+++ precache-manifest.e06796a1723de28a181d58511be179cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a831b8ae92752c67823a6192eab55862",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "4a32b791984d34838c2f",
+    "url": "/todo-app/static/css/main.6bf6281c.chunk.css"
+  },
+  {
+    "revision": "3d2c105bec7e00f7463f",
+    "url": "/todo-app/static/js/2.69f54fe6.chunk.js"
+  },
+  {
+    "revision": "828f33f734ffe63111c0706841f09727",
+    "url": "/todo-app/static/js/2.69f54fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2bbd9737e4c89d16c79",
+    "url": "/todo-app/static/js/3.213a1e04.chunk.js"
+  },
+  {
+    "revision": "4a32b791984d34838c2f",
+    "url": "/todo-app/static/js/main.8f7c9955.chunk.js"
+  },
+  {
+    "revision": "553b09cb79ed043a66e0",
+    "url": "/todo-app/static/js/runtime-main.11857d72.js"
+  }
+]);
